47ad554d26032340c46b68d929be02abfc2dc582
Chr1ssy <chrischros@live.de>
2020-01-23 10:05:57 +0100

    Update gateway.js
    Removal of tokens/keys and highlighting of interfaces that need to be changed.

--- gateway.js
+++ gateway.js
@@ -1,11 +1,11 @@
-// Beuthbot [Gateway]
+// BeuthBot [Gateway]
 // contributed by Christopher Lehmann and Timo Bruns
-// v 1.0 - 01/12/2020
+// v 2.1 - 01/23/2020
 
 /**
-* Doc:		Each message contains "text" and a "chat" object, which has an "id" which is the chat id.
-* Bot:		cbeuthbot | 916877791:AAEcIVYenYLjbjt7xzAr4fan6SlDIxzlDz4
-* URL:		"https://api.telegram.org/bot<API_token>/sendMessage"
+* Doc:		Each message [req.body.data.message] contains e.g. "text" and a "chat" object, which has an "id" that is the chat id.
+* Bot:		<bot> | 123456789:ABCdEFGhiJKlmno1pqRs2tuv3W4XYzabCd5 [sample token]
+* URL:		"https://api.telegram.org/bot<API_token>/sendMessage" [reply]
 * cURL:		curl -F "url=https://my-telegram-bot.beuthbot.now.sh/message-in" https://api.telegram.org/bot916877791:AAEcIVYenYLjbjt7xzAr4fan6SlDIxzlDz4/setWebhook
 * Deploy:	https://zeit.co/beuthbot | https://zeit.co/beuthbot/my-telegram-bot/piw9yrfez [ZEIT | <now>]
 */
@@ -28,24 +28,22 @@ app.use(
 
 // the route the API will call:
 app.post('/message-in', function(req, res) {
-	//console.log(typeof req.body)
-	//console.log(req.body)
 	const message = req.body.data.message // message object
 
 	if (!message || message.text.length < 1) { // if message is not present or message text is empty
 		return res.end() // return empty response
 	}
 	
-	const options = { // options object (especially headers for POST)
-		headers: {
+	const options = { // options object (specific headers for Azure POST)
+		headers: { // Ocp-Apim-Subscription-Key | 12abc34567de8910f123456g78h9i1j0 [sample key]
 			'Host': 'northeurope.api.cognitive.microsoft.com',
 			'Content-Type': 'application/json',
-			'Ocp-Apim-Subscription-Key': '54dbb69669be4638b704460c96e8c8f3'
+			'Ocp-Apim-Subscription-Key': '<key>'
 		}
 	}
-	
-	console.debug(message)
 
+	// TODO replace Azure with Rasa [or adapter]
+	
 	axios
 	.post( // POST -> Microsoft Azure - Cognitive Services | NLU | (Response = json[entities, content, score, ...])
 		'https://northeurope.api.cognitive.microsoft.com/text/analytics/v2.1/sentiment',
@@ -63,12 +61,16 @@ app.post('/message-in', function(req, res) {
     console.log(error);
 	})
 	
-	.then(function (response) {
+	// TODO send response to registry
+	
+	.then(function (response) { // reply to user [chat]
 		message_out = "[" + message.chat.id + "]: " + "Hi, your score is " + response.data.documents[0].score + "."
+		
+		// TODO adjust answer mechanism
 		
 		axios
 		.post( // nested chat answer to avoid missing Azure response
-			'https://api.telegram.org/bot1008019346:AAHZ-PALIP0-TcCq5r90u3A5ekc1JvbdTy4/sendMessage',
+			'https://api.telegram.org/bot<token>/sendMessage',
 			{
 				chat_id: message.chat.id,
 				text: message_out
@@ -79,8 +81,6 @@ app.post('/message-in', function(req, res) {
 			console.log('Message posted')
 			res.end('ok')
 		})
-		
-		//console.log(message_out)
 	})
 })
 
