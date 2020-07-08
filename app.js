9691e40a67074af04b35ab1a17f9b7417b729d6e
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2020-07-08 23:34:22 +0200

    do not send min_confidence_score to deconcentrator

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ expressApp.post('/message', function(req, res) {
     const deconcentratorMessage = {}
     deconcentratorMessage.text = text
 
-    deconcentratorMessage.min_confidence_score = 0.50
+    // deconcentratorMessage.min_confidence_score = 0.50
     deconcentratorMessage.processors = ["rasa"]
     deconcentratorMessage.history = ["gateway"]
 
