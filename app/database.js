3e5c77dadf4d635c29d757d0a6b24cb842b2b21b
s79912 <s79912@beuth-hochschule.de>
2020-11-12 00:30:08 +0100

    fix gateway dependend to tegram-id - due to database throwing exception when no user stored

--- app/database.js
+++ app/database.js
@@ -12,15 +12,20 @@ const util = require('util')
 async function getUser(message) {
 
     console.debug("getUser message:\n" + util.inspect(message, false, null, true) + "\n\n")
-    const userCandidateResponse = await axios.post(endpoint, message)
 
-    if (userCandidateResponse && userCandidateResponse.data) {
-        console.debug("databases response:\n" + util.inspect(userCandidateResponse.data, false, null, true) + "\n\n")
-        return userCandidateResponse.data
-    } else {
-        console.debug("no userCandidateResponse found")
+    try {
+        const userCandidateResponse = await axios.post(endpoint, message)
+
+        if (userCandidateResponse && userCandidateResponse.data) {
+            console.debug("databases response:\n" + util.inspect(userCandidateResponse.data, false, null, true) + "\n\n")
+            return userCandidateResponse.data
+        }
+    }
+    catch (e) {
+        /* ignored exception, probably 404 */
     }
 
+    console.debug("no userCandidateResponse found")
     return null
 }
 
