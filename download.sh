3db0cb67c42e52f9b51647bc8e7e6f99ab1c9af0
RobertHalwass <30471969+RobertHalwass@users.noreply.github.com>
2021-02-09 17:58:56 +0100

    convert to ogg opus

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 apt-get update
-apt-get install ffmpeg -y
+
 apt-get install libogg0 -y
 apt-get install libopus0 -y
 apt-get install opus-tools -y
 apt-get install sox -y
-apt-get install libav-tools -y
+apt-get install ffmpeg -y
